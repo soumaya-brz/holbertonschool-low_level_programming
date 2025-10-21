389217d19118fefec455c5e05d8278fd81c93d56
soumaya <soumayabraziii@gmail.com>
2025-10-21 07:26:53 +0000

    feat: print the alphabet in lowercase except q and e

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase except q et e
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+	if (ch != 'e' && ch != 'q')
+	putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
