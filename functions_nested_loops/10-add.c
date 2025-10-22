09afd5de1344a92af7a8ad8e2d5c7a7a193b0f7f
soumaya <soumayabraziii@gmail.com>
2025-10-22 15:19:59 +0000

    feat:  adds two integers and returns the result

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Return: the sum of a and b
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
