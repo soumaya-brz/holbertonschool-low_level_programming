390621fad62ee9c90d6fe8cadc7eef9f67e48299
soumaya <soumayabraziii@gmail.com>
2025-10-27 14:01:28 +0000

    fix: correction betty

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -7,23 +7,23 @@
  */
 int main(void)
 {
-    int i;
+int i;
 
-    for (i = 1; i <= 100; i++)
-    {
-        if (i % 3 == 0 && i % 5 == 0)
-            printf("FizzBuzz");
-        else if (i % 3 == 0)
-            printf("Fizz");
-        else if (i % 5 == 0)
-            printf("Buzz");
-        else
-            printf("%d", i);
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+printf("FizzBuzz");
+else if (i % 3 == 0)
+printf("Fizz");
+else if (i % 5 == 0)
+printf("Buzz");
+else
+printf("%d", i);
 
-        if (i < 100)
-            printf(" "); /* espace entre les mots/nombres */
-    }
+if (i < 100)
+printf(" ");
+}
 
-    printf("\n");
-    return (0);
+printf("\n");
+return (0);
 }
