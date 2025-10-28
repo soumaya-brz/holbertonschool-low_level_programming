60caf89850901465fec696801013c213cc05e93d
soumaya <soumayabraziii@gmail.com>
2025-10-28 14:22:23 +0000

    fix: correction betty

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -5,11 +5,11 @@
  *
  * Return: void
  */
-void puts2(char *str);
+void puts2(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\n')
+	while (str[i] != '\0')
 	{
 		if (i % 2 == 0)
 			_putchar(str[i]);
