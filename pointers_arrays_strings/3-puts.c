410f7b0a9f6dc0a42227542afdd52e4c88620e73
soumaya <soumayabraziii@gmail.com>
2025-10-28 10:23:38 +0000

    feat:  prints a string

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - prints a string
+ * @str: pointer to the string to display
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '0/')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
