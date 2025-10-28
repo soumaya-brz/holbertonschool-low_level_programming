759f1208a4f3e31e04be87e3fd0b5dd62ecc5228
soumaya <soumayabraziii@gmail.com>
2025-10-28 09:38:30 +0000

    feat: returns the length of a string

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * int _strlen - returns the length of a string
+ * @s: pointeur vers la chaîne
+ *
+ * Return: longueur de la chaîne
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return len;
+}
