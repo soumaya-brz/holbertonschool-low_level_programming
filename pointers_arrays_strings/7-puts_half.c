ba306616d94ef6cec6bc86efce510caf814098f8
soumaya <soumayabraziii@gmail.com>
2025-10-28 14:41:37 +0000

    feat: prints half of a string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: pointer to the string
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int len = 0
+	int i, start;
+
+	while (str[len] != '\0')
+		len++;
+
+	if (len % 2 == 0)
+		star = len / 2;
+	else
+		start = (len + 1) / 2;
+
+	for (i = start; i < len; i++)
+		_putchar(str[i]);
+
+	_putchar('/n')
+}
