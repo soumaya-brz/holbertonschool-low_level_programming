e6573b3aca8a0034a517a7b0968b1ac61f7f1712
soumaya <soumayabraziii@gmail.com>
2025-10-29 12:47:50 +0000

    feat: concatenates two strings appends at most n bytes from src to the end of dest

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncat - appends at most n bytes from src to the end of dest
+ * @dest: pointer to the destination string
+ * @src: pointer to the source string
+ * @n: maximum number of type to append
+ *
+ * Return: the pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i = 0, int j = 0;
+
+while (dest[i] != '0\');
+i++
+	while (src[j] != '0\' && j < n)
+		dest[i] = src[j];
+i++
+	j++
+}
+
+dest[i] = '\0';
+
+return (dest);
+}
