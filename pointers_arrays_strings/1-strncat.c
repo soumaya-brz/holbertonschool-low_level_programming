29d75a3b38eb934c5c3b8bd0e71143443521751f
soumaya <soumayabraziii@gmail.com>
2025-10-29 12:51:39 +0000

    fix: correct compile program

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -9,17 +9,19 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-int i = 0, int j = 0;
+int i = 0, j = 0;
 
-while (dest[i] != '0\');
-i++
-	while (src[j] != '0\' && j < n)
-		dest[i] = src[j];
-i++
-	j++
-}
+    while (dest[i] != '\0')
+        i++;
 
-dest[i] = '\0';
+    while (src[j] != '\0' && j < n)
+    {
+        dest[i] = src[j];
+        i++;
+        j++;
+    }
 
-return (dest);
-}
+    dest[i] = '\0';
+
+    return dest;
+}	
