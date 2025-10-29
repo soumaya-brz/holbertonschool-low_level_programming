5985b462051951a9ce4ca5540c3e7cac87fb6090
soumaya <soumayabraziii@gmail.com>
2025-10-29 13:13:45 +0000

    fix: compile program correct

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -11,7 +11,7 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while (i < n && src[i] != '0\')
+	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
@@ -19,7 +19,7 @@ char *_strncpy(char *dest, char *src, int n)
 
 	while (i < n)
 	{
-		dest[i] = '0\';
+		dest[i] = '\0';
 		i++
 	}
 	return (dest);
