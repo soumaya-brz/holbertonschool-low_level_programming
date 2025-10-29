5197a14f9a0255d8bbc69a48ed87d7ce9219d106
soumaya <soumayabraziii@gmail.com>
2025-10-29 12:56:36 +0000

    fix: correct betty coding style

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -11,17 +11,17 @@ char *_strncat(char *dest, char *src, int n)
 {
 int i = 0, j = 0;
 
-    while (dest[i] != '\0')
-        i++;
+while (dest[i] != '\0')
+	i++;
 
-    while (src[j] != '\0' && j < n)
-    {
-        dest[i] = src[j];
-        i++;
-        j++;
-    }
+while (src[j] != '\0' && j < n)
+{
+	dest[i] = src[j];
+	i++;
+	j++;
+}
 
-    dest[i] = '\0';
+dest[i] = '\0';
 
-    return dest;
-}	
+return (dest);
+}
