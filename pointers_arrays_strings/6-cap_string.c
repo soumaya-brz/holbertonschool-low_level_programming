b48fb9431c4f9a4ac6ab3bfdb4579cc9f6816450
soumaya <soumayabraziii@gmail.com>
2025-10-29 14:39:19 +0000

    feat: capitalizes all words of a string

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: pointer to the string to modify
+ *
+ * Return: pointer to the modified string
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+	int j;
+	char separators[] = " \t\n,;.!?\"(){}";
+
+	if (s[0] >= 'a' && s[0] <= 'z')
+		s[0] = s[0] - 32;
+
+	while (s[i] != '\0')
+	{
+		for (j = 0; separators[j] != '\0')
+		{
+			if (s[i] == separators[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+				s[i + 1] = s[i + 1] - 32;
+				}
+			}
+			i++;
+		}
+		return (s);
+	}
