c226309e3695ecb4121c9004733d216de1282843
soumaya <soumayabraziii@gmail.com>
2025-11-03 16:53:29 +0000

    feat: length of a prefix substring.

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string to check
+ * @accept: allowed characters
+ *
+ * Return: number of bytes in the initial segment of s
+ *         which consist only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	char *p;
+	unsigned int count = 0;
+	int found;
+
+	while (*s)
+	{
+		found = 0;
+
+		for (p = accept; *p; p++)
+		{
+			if (*s == *p)
+			{
+				found = 1
+				break;
+			}
+		}
+
+		if (!found)
+		break;
+
+		count++;
+		s++;
+	}
+
+	return (count);
+}
