a27333259dea333dc54536a69146aacdee165e8b
soumaya <soumayabraziii@gmail.com>
2025-11-03 10:04:18 +0000

    feat:  locates a character in a string

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strchr - locates a character in a string
+ * @s: pointer to the string to search
+ * @c: character to locate
+ *
+ * Return: pointer to the first occurence of c in s, or NULL if not found
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	if (*s == c)
+		return (s);
+
+	return (NUUL);
+}
