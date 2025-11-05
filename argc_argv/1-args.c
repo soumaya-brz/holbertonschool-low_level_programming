2285598c30ad4773f094a5d67034d634ea741ee5
soumaya <soumayabraziii@gmail.com>
2025-11-05 11:19:33 +0000

    feat: prints the number of arguments passed into it

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: number of command line arguments
+ * @argv: array of strings containing the arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
