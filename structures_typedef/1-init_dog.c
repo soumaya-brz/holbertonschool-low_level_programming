69782383d2680f700fb25a2f166fae08b29050e3
soumaya <soumayabraziii@gmail.com>
2025-11-12 12:21:56 +0000

    fix: declaration of NULL

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
 #include "dog.h"
-
+#include <stddef.h>
 /**
  * init_dog - initialize a variable of type struct dog
  * @d: pointer to a struct dog
@@ -12,9 +12,10 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
-    {
-	    d->name = name;
+
+	{
+	d->name = name;
 	d->age = age;
 	d->owner = owner;
-    }
+	}
 }
