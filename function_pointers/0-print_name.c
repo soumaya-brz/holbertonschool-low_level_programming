5df259b8e12f8a3c858f95487f69827d0d5b77a9
soumaya <soumayabraziii@gmail.com>
2025-11-17 14:10:55 +0000

    fix: correct betty style

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,5 +1,13 @@
 #include "function_pointers.h"
-
+/**
+ * print_name - prints a name using a given function
+ * @name: the name to print
+ * @f: pointer to a function that takes a string as parameter and
+ * return void
+ *
+ * this function calls the function pointed to by f,
+ * passing the string name to it.
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != 0 && f != 0)
