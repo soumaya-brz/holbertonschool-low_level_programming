6e0e99200fd43b2369db539bfa90e176be681058
soumaya <soumayabraziii@gmail.com>
2025-11-17 16:10:49 +0000

    fix : correct compilation

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -1,7 +1,7 @@
 #ifndef CALC_H
 #define CALC_H
 
-#include <stdlib>
+#include <stdlib.h>
 
 typedef struct op
 {
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "3-calc.c"
+#include "3-calc.h"
 
 /**
  * main - performs simple operations
