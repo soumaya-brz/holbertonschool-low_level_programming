f3651bc949f6118de6d20b3a6f6720f361f265fe
soumaya <soumayabraziii@gmail.com>
2025-12-01 15:12:09 +0000

    fix: strdup declaration

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -29,5 +29,6 @@ void free_list(list_t *head);
 int _putchar(char c);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
+char *strdup(const char *s);
 
 #endif /* LISTS_H */
