65c03c08e3177b62c7dc236373135801814d4dcd
soumaya <soumayabraziii@gmail.com>
2025-12-02 10:53:11 +0000

    fix: kernel style code correction

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 
 /**
- * delete_dnodeint_at_index - deletes the node at index index of a dlistint_t list
- *
+ * delete_dnodeint_at_index - deletes the node at a given index
  * @head: pointer to the head of the list
  * @index: index of the node to delete (starts at 0)
  *
@@ -11,34 +10,34 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *temp;
+	dlistint_t *current;
 	unsigned int i;
 
-	if (!head || !*head)
+	if (head == NULL || *head == NULL)
 		return (-1);
 
-	temp = *head;
+	current = *head;
 
 	if (index == 0)
 	{
-		*head = temp->next;
-		if (temp->next)
-			temp->next->prev = NULL;
-		free(temp);
+		*head = current->next;
+		if (current->next != NULL)
+			current->next->prev = NULL;
+		free(current);
 		return (1);
 	}
 
-	for (i = 0; temp && i < index; i++)
-		temp = temp->next;
+	for (i = 0; current != NULL && i < index; i++)
+		current = current->next;
 
-	if (!temp)
+	if (current == NULL)
 		return (-1);
 
-	if (temp->prev)
-		temp->prev->next = temp->next;
-	if (temp->next)
-		temp->next->prev = temp->prev;
+	if (current->prev != NULL)
+		current->prev->next = current->next;
+	if (current->next != NULL)
+		current->next->prev = current->prev;
 
-	free(temp);
+	free(current);
 	return (1);
 }
