c9a2c088220b683a609ddb329662717bbd2b8a0a
soumaya <soumayabraziii@gmail.com>
2025-12-04 14:50:23 +0000

    feat: appends text at the end of a file

--- file_io/main.h
+++ file_io/main.h
@@ -6,6 +6,7 @@
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
 /* Prototype for _putchar function */
 int _putchar(char c);
