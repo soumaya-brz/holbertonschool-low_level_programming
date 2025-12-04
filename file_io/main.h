9d35f79852e66b6826c72a9020a86d6566495b25
soumaya <soumayabraziii@gmail.com>
2025-12-04 14:48:49 +0000

    feat: creates a file

--- file_io/main.h
+++ file_io/main.h
@@ -4,8 +4,8 @@
 #include <stddef.h>
 #include <unistd.h>
 
-/* Prototype for the read_textfile function */
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 /* Prototype for _putchar function */
 int _putchar(char c);
